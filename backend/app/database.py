a4b86ca2fac13b38f374949d0b55404c20ca1bb7
kethashishir <kethashishir@gmail.com>
2026-04-18 10:12:26 -0700

    refactor database base and config

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,10 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
 
 load_dotenv()
 database_url = os.getenv("DATABASE_URL")
