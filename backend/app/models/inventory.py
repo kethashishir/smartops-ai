4c498e2741a11bd03843a4f72f470dbce7ed0a90
kethashishir <kethashishir@gmail.com>
2026-04-18 10:12:02 -0700

    Inventory  model created

---
+++ backend/app/models/inventory.py
@@ -0,0 +1,17 @@
+# inventory
+# - id
+# - product_id
+# - current_stock
+# - updated_at
+
+from app.database import Base
+from sqlalchemy import Column, DateTime, Integer, ForeignKey, func, onup
+
+class Inventory(Base):
+    __tablename__ = "inventory"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(Integer, ForeignKey("products.id"), nullable=False, unique=True)
+    current_stock = Column(Integer, nullable=False)
+    updated_at = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+
