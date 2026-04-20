4160b08d731ad025eda3fa9dcea11c27a5906d7e
kethashishir <kethashishir@gmail.com>
2026-04-19 21:44:48 -0700

    add order router and inventory update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.routers import products, inventory
+from app.routers import products, inventory, order
 from app.database import Base, engine
-from app.models import product, orders, inventories, forecast, recommendation, user  # Ensure the Product and Category models are imported so they're registered with SQLAlchemy
+from app.models import product, inventories, forecast, recommendation, user, orders  # Ensure the Product and Category models are imported so they're registered with SQLAlchemy
 
 app = FastAPI()
 
@@ -10,7 +10,7 @@ async def health():
     return {"status": "healthy"}
 
 app.include_router(products.router)
-
+app.include_router(order.router)
 app.include_router(inventory.router)
 
 @app.get("/")
---
+++ backend/app/routers/order.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models.orders import Order
+from app.models.inventories import Inventory
+from app.models.product import Product
+from app.schemas.orders import OrderCreate, OrderResponse
+
+router = APIRouter(prefix="/orders", tags=["orders"])
+
+@router.post("/", response_model=OrderResponse)
+def create_order(order: OrderCreate, db: Session = Depends(get_db)):
+    
+    db_inventory = db.query(Inventory).filter(Inventory.product_id == order.product_id).first()
+
+    if not db.query(Product).filter(Product.id == order.product_id).first():
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    if not db_inventory:
+        raise HTTPException(status_code=404, detail="Inventory record not found for the product")
+
+    if db_inventory.current_stock < order.quantity:
+        raise HTTPException(status_code=400, detail="Not enough stock")
+
+    
+    if db_inventory:
+        db_inventory.current_stock -= order.quantity
+        db.commit()
+        db.refresh(db_inventory)
+
+    db_order = Order(
+        product_id=order.product_id,
+        quantity=order.quantity,
+        source=order.source
+    )
+    db.add(db_order)
+    db.commit()
+    db.refresh(db_order)
+
+    return db_order
+
+@router.get("/", response_model=list[OrderResponse])
+def get_orders(db: Session = Depends(get_db)):
+    orders = db.query(Order).all()
+
+    return orders 
