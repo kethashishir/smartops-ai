b89f5faa16f093b3b7cad896c0cff80bff6efeb7
kethashishir <kethashishir@gmail.com>
2026-04-20 09:27:18 -0700

    add recommendation router and main update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.models import product, inventories, forecast, recommendation, user, orders  # Ensure the Product and Category models are imported so they're registered with SQLAlchemy
-from app.routers import products, inventory, order, forecast
+from app.routers import products, inventory, order, forecast, recommendation
 
 app = FastAPI()
 
@@ -13,6 +13,7 @@ app.include_router(products.router)
 app.include_router(order.router)
 app.include_router(inventory.router)
 app.include_router(forecast.router)
+app.include_router(recommendation.router)
 
 @app.get("/")
 async def root():
