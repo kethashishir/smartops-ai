0783efd9e8a499d1cd9db5e954e4cf9919eac527
kethashishir <kethashishir@gmail.com>
2026-05-10 20:44:03 -0700

    First API call

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const products = [
-    {
-      id: 1,
-      name: 'Laptop',
-      description: 'A high-performance laptop for all your computing needs.'
-    },
-    {
-      id: 2,
-      name: 'Mouse',
-      description: 'A wireless mouse with ergonomic design and long battery life.'
-    },
-    {
-      id: 3,
-      name: 'Keyboard',
-      description: 'A mechanical keyboard with customizable RGB lighting and tactile feedback.'
+
+  const [products, setProducts] = useState([]);
+
+  async function fetchProducts() {
+    try {
+      console.log("Fetching products...");
+      const response = await fetch("http://127.0.0.1:8000/products/");
+      console.log("Response status:", response.status);
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log("Products fetched:", data);
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products:", error.message);
     }
-  ];
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
 
   return (
     <div>
@@ -30,13 +36,16 @@ function App() {
       <ul>
         {products.map((product) => (
           <li key={product.id}>
+            <h2>{product.id}</h2>
+            <h2>{product.sku}</h2>
             <h2>{product.name}</h2>
-            <p>{product.description}</p>
+            <h2>{product.category}</h2>
+            <h2>{product.unit_price}</h2>
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
