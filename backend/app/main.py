a0171f03b48dea6cc3ed5b5f84d95cd56dfdf9b3
kethashishir <kethashishir@gmail.com>
2026-06-09 08:58:35 -0700

    FIxed CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.models import product, inventories, forecast, recommendation, user, orders  # Ensure the Product and Category models are imported so they're registered with SQLAlchemy
 from app.routers import products, inventory, order, forecast, recommendation
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 async def health():
